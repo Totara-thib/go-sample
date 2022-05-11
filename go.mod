4cf95853905e5667b9507377a0a09e5fab99d0c9
Protocole <burgoni@pm.me>
2022-05-11 14:18:06 +0200

    Add simple go project
    Signed-off-by: Protocole <burgoni@pm.me>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go/sample
+
+go 1.18
+
+require github.com/google/go-github/v44 v44.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	helloWorld = "Hello World"
+)
+
+func main() {
+	println("🐬")
+}
